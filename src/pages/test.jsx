c0cbf446b1138ec4dc3bfcf4b1979469bcbe08bd
ykdojo <yksu4p@gmail.com>
2022-09-12 19:46:20 -0700

    initial commit

---
+++ src/pages/test.jsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/Button'
+
+export default function Login() {
+  return (
+    <>
+      <Button href="/register" color="white" className="mt-10">
+        <span className="text-blue-600">Join us on Discord</span>
+      </Button>
+    </>
+  )
+}
